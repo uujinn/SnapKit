5877b119369690749d66c3713402eacecbe0363a
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-10 20:56:26 +0900

    [FEAT] No Storyboard Setting

---
+++ ProjectWithoutStoryboard/ProjectWithoutStoryboard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ProjectWithoutStoryboard
+//
+//  Created by 양유진 on 2022/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "ViewController No Storyboard"
+        self.view.backgroundColor = .orange
+    }
+
+
+}
+
